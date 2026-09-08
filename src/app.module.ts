60a227c4f8d8e6acc54802a079d03a6cb5607e47
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 22:57:28 +0300

    feat: register tenant WhatsApp phone numbers

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ContactsModule } from "./contacts/contacts.module.js";
 import { HealthModule } from "./health/health.module.js";
 import { MessagesModule } from "./messages/messages.module.js";
 import { OutboxModule } from "./outbox/outbox.module.js";
+import { PhoneNumbersModule } from "./phone-numbers/phone-numbers.module.js";
 import { PrismaModule } from "./prisma/prisma.module.js";
 import { QueueModule } from "./queue/queue.module.js";
 import { WebhooksModule } from "./webhooks/webhooks.module.js";
@@ -15,6 +16,7 @@ import { WebhooksModule } from "./webhooks/webhooks.module.js";
     PrismaModule,
     AuthModule,
     ContactsModule,
+    PhoneNumbersModule,
     QueueModule,
     OutboxModule,
     HealthModule,
