ad28e85ab5f118db495cb50c8615e6d4ef1de8ce
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 23:00:59 +0300

    feat: register inbound WhatsApp message processor

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { PhoneNumbersModule } from "../phone-numbers/phone-numbers.module.js";
+import { InboundMessageService } from "./inbound-message.service.js";
 import { WebhookEventProcessorService } from "./webhook-event-processor.service.js";
 import { WebhookStatusService } from "./webhook-status.service.js";
 import { WebhooksController } from "./webhooks.controller.js";
 
 @Module({
+  imports: [PhoneNumbersModule],
   controllers: [WebhooksController],
-  providers: [WebhookStatusService, WebhookEventProcessorService],
+  providers: [InboundMessageService, WebhookStatusService, WebhookEventProcessorService],
 })
 export class WebhooksModule {}
