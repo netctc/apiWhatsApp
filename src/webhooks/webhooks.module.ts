89cfd10682f34fcdce2e2aa8554f0030cbafb9b8
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 21:39:09 +0300

    feat: add Meta webhook module

---
+++ src/webhooks/webhooks.module.ts
@@ -0,0 +1,7 @@
+import { Module } from "@nestjs/common";
+import { WebhooksController } from "./webhooks.controller.js";
+
+@Module({
+  controllers: [WebhooksController],
+})
+export class WebhooksModule {}
