31d1a05afcbc39ac32ab6b3069dd1b13709b40b2
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 22:10:51 +0300

    feat: wire authentication into messages module

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module.js";
 import { MessagesController } from "./messages.controller.js";
 import { MessagesService } from "./messages.service.js";
 
 @Module({
+  imports: [AuthModule],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
