800ecdb3461a594bccffdab5d00c41c770e382ce
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 21:47:58 +0300

    feat: register webhook status processor

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { WebhookStatusService } from "./webhook-status.service.js";
 import { WebhooksController } from "./webhooks.controller.js";
 
 @Module({
   controllers: [WebhooksController],
+  providers: [WebhookStatusService],
 })
 export class WebhooksModule {}
