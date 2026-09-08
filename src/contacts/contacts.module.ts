feff76d1c5e8c6de94083f54e02ecd64eec62be6
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 22:24:37 +0300

    feat: add contacts and consent module

---
+++ src/contacts/contacts.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module.js";
+import { ContactsController } from "./contacts.controller.js";
+import { ContactsService } from "./contacts.service.js";
+
+@Module({
+  imports: [AuthModule],
+  controllers: [ContactsController],
+  providers: [ContactsService],
+  exports: [ContactsService],
+})
+export class ContactsModule {}
