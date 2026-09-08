05fcaa072a8d94568a2eb89f2d8e768ec7b5c534
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 22:58:01 +0300

    feat: register tenant Meta sender resolver

--- src/meta/meta.module.ts
+++ src/meta/meta.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PhoneNumbersModule } from "../phone-numbers/phone-numbers.module.js";
+import { MetaSenderResolverService } from "./meta-sender-resolver.service.js";
 import { MetaWhatsAppClient } from "./meta-whatsapp.client.js";
 
 @Module({
-  providers: [MetaWhatsAppClient],
-  exports: [MetaWhatsAppClient],
+  imports: [PhoneNumbersModule],
+  providers: [MetaWhatsAppClient, MetaSenderResolverService],
+  exports: [MetaWhatsAppClient, MetaSenderResolverService],
 })
 export class MetaModule {}
