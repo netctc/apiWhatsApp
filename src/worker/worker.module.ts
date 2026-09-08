0b6ad43112a4d287c79b5f111d4d9c1bf2cd46dc
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 21:47:20 +0300

    feat: add dedicated worker module

---
+++ src/worker/worker.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MetaModule } from "../meta/meta.module.js";
+import { PrismaModule } from "../prisma/prisma.module.js";
+import { QueueModule } from "../queue/queue.module.js";
+import { DistributedRateLimiterService } from "./distributed-rate-limiter.service.js";
+import { MessageDispatcherService } from "./message-dispatcher.service.js";
+import { OutboundWorkerService } from "./outbound-worker.service.js";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    QueueModule,
+    MetaModule,
+  ],
+  providers: [
+    DistributedRateLimiterService,
+    MessageDispatcherService,
+    OutboundWorkerService,
+  ],
+})
+export class WorkerModule {}
