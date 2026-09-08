bdaecdecc60d333df56b6b450813ea05a51d2e89
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 21:48:20 +0300

    feat: enable asynchronous webhook processing

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
+import { WebhookEventProcessorService } from "./webhook-event-processor.service.js";
 import { WebhookStatusService } from "./webhook-status.service.js";
 import { WebhooksController } from "./webhooks.controller.js";
 
 @Module({
   controllers: [WebhooksController],
-  providers: [WebhookStatusService],
+  providers: [WebhookStatusService, WebhookEventProcessorService],
 })
 export class WebhooksModule {}
