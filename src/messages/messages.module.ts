e0f08bcc5bc20ef6c493a2f85a0127be45f0bf86
netctc <36329039+netctc@users.noreply.github.com>
2026-09-08 22:59:46 +0300

    feat: resolve tenant senders from messages module

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ContactsModule } from "../contacts/contacts.module.js";
+import { PhoneNumbersModule } from "../phone-numbers/phone-numbers.module.js";
 import { MessagesController } from "./messages.controller.js";
 import { MessagesService } from "./messages.service.js";
 import { OutboundPolicyService } from "./outbound-policy.service.js";
 
 @Module({
-  imports: [ContactsModule],
+  imports: [ContactsModule, PhoneNumbersModule],
   controllers: [MessagesController],
   providers: [MessagesService, OutboundPolicyService],
 })
