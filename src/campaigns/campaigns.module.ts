d10d3f9f4663489532b61fb42200a8fb52769600
netctc <36329039+netctc@users.noreply.github.com>
2026-09-09 18:04:01 +0300

    feat: add campaign foundation module

---
+++ src/campaigns/campaigns.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { MessagesModule } from "../messages/messages.module.js";
+import { PhoneNumbersModule } from "../phone-numbers/phone-numbers.module.js";
+import { PrismaModule } from "../prisma/prisma.module.js";
+import { TemplatesModule } from "../templates/templates.module.js";
+import { CampaignProcessorService } from "./campaign-processor.service.js";
+import { CampaignsController } from "./campaigns.controller.js";
+import { CampaignsService } from "./campaigns.service.js";
+
+@Module({
+  imports: [PrismaModule, PhoneNumbersModule, TemplatesModule, MessagesModule],
+  controllers: [CampaignsController],
+  providers: [CampaignsService, CampaignProcessorService],
+  exports: [CampaignsService],
+})
+export class CampaignsModule {}
