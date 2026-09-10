ffd603f854253f806e43b37a886361a1c975d044
netctc <36329039+netctc@users.noreply.github.com>
2026-09-10 19:05:03 +0300

    feat: wire media storage into readiness

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { MediaModule } from "../media/media.module.js";
 import { HealthController } from "./health.controller.js";
 import { OperationalHealthService } from "./operational-health.service.js";
 
 @Module({
+  imports: [MediaModule],
   controllers: [HealthController],
   providers: [OperationalHealthService],
   exports: [OperationalHealthService],
