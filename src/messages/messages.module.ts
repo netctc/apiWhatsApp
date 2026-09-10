5451d947e6e48835fbdc877ac23fe5bc760a11e3
netctc <36329039+netctc@users.noreply.github.com>
2026-09-10 11:24:49 +0300

    feat: connect messages to inbox conversations

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ContactsModule } from "../contacts/contacts.module.js";
+import { InboxModule } from "../inbox/inbox.module.js";
 import { PhoneNumbersModule } from "../phone-numbers/phone-numbers.module.js";
 import { TemplatesModule } from "../templates/templates.module.js";
 import { MessagesController } from "./messages.controller.js";
@@ -7,7 +8,7 @@ import { MessagesService } from "./messages.service.js";
 import { OutboundPolicyService } from "./outbound-policy.service.js";
 
 @Module({
-  imports: [ContactsModule, PhoneNumbersModule, TemplatesModule],
+  imports: [ContactsModule, InboxModule, PhoneNumbersModule, TemplatesModule],
   controllers: [MessagesController],
   providers: [MessagesService, OutboundPolicyService],
   exports: [MessagesService],
