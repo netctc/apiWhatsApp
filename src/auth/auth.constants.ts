5241c72ef0bd2e3e270dab6575313c14e74d2f5b
netctc <36329039+netctc@users.noreply.github.com>
2026-09-10 16:42:27 +0300

    feat: add media read scope

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -4,6 +4,7 @@ export const REQUIRED_SCOPES_KEY = "auth:requiredScopes";
 export enum ApiScope {
   MESSAGES_READ = "messages:read",
   MESSAGES_WRITE = "messages:write",
+  MEDIA_READ = "media:read",
   MEDIA_WRITE = "media:write",
   CONTACTS_READ = "contacts:read",
   CONTACTS_WRITE = "contacts:write",
