2e746d71a05c2c48f32875735deb0fcb9f7837d7
netctc <36329039+netctc@users.noreply.github.com>
2026-09-10 06:00:18 +0300

    feat: export Meta media client

--- src/meta/meta.module.ts
+++ src/meta/meta.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { PhoneNumbersModule } from "../phone-numbers/phone-numbers.module.js";
+import { MetaMediaClient } from "./meta-media.client.js";
 import { MetaSenderResolverService } from "./meta-sender-resolver.service.js";
 import { MetaTemplateClient } from "./meta-template.client.js";
 import { MetaWhatsAppClient } from "./meta-whatsapp.client.js";
 
 @Module({
   imports: [PhoneNumbersModule],
-  providers: [MetaWhatsAppClient, MetaSenderResolverService, MetaTemplateClient],
-  exports: [MetaWhatsAppClient, MetaSenderResolverService, MetaTemplateClient],
+  providers: [MetaWhatsAppClient, MetaSenderResolverService, MetaTemplateClient, MetaMediaClient],
+  exports: [MetaWhatsAppClient, MetaSenderResolverService, MetaTemplateClient, MetaMediaClient],
 })
 export class MetaModule {}
