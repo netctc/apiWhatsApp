49029b14c83af76cc9d0b57c6317f8f445ad547b
netctc <36329039+netctc@users.noreply.github.com>
2026-09-11 20:35:01 +0300

    feat: wire realtime events into inbox module

--- src/inbox/inbox.module.ts
+++ src/inbox/inbox.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { InboxEventsModule } from "../inbox-events/inbox-events.module.js";
 import { ConversationActivityService } from "./conversation-activity.service.js";
 import { ConversationNotesController } from "./conversation-notes.controller.js";
 import { ConversationNotesService } from "./conversation-notes.service.js";
@@ -6,6 +7,7 @@ import { InboxController } from "./inbox.controller.js";
 import { InboxService } from "./inbox.service.js";
 
 @Module({
+  imports: [InboxEventsModule],
   controllers: [InboxController, ConversationNotesController],
   providers: [InboxService, ConversationActivityService, ConversationNotesService],
   exports: [InboxService, ConversationActivityService],
